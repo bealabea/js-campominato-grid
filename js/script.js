3d06dba9e9c325dabf5e89844f0ce2282bf701a7
bealabea <beatricemarin89@yahoo.it>
2021-12-18 18:42:54 +0100

    exercise redone with HTML buttons

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-
-// Creo una funzione per la creazione dei button per ogni livello del gioco
-function levels (buttonOutput, buttonClass, textButton){
-    const levels = document.createElement('button')
-    levels.className = 'button', buttonClass;
-    levels.innerHTML = textButton;
-    buttonOutput.append(levels);
-    levels.addEventListener('click', function(){
-        buildGrid();
-    })
-}
+// Creo una funzione per stampare il numero all'interno di ogni box che andrò a creare
+function boxNumber(newSquare, num){
+    newSquare.innerHTML = num;
+};
 
 // Creo una funzione per creare un box e la colorazione del box al click
-function createNewBox(output){
-    const newSquare = document.createElement('div');
-    newSquare.className = 'box', 'show';
+function createNewBox (output, num) {
+    let newSquare = document.createElement('div');
+    newSquare.className ='box';
     output.append(newSquare);
-    newSquare.addEventListener('click', function () {
-        this.classList.toggle('blue');
+    boxNumber(newSquare, num);
+    newSquare.addEventListener('click', function(){
+    this.classList.toggle('blue');   
     })
 }
 
-function buildGrid() {
-    outputHtml.innerHTML = '';
-    for (i = 1; i <= max; i++) {
-        createNewBox(output);
+// Creo la funzione di un ciclo for per la creazione di n box
+function buildGrid(output, maxNum, width){
+    output.innerHTML = '';
+    output.className = width;
+    for (let i = 0; i < maxNum; i++) {
+        createNewBox(output, i+1); 
     }
 }
 
-const outputHtml = document.getElementById('output');
+// Assegno le variabili per i 3 button
+const levelOne = document.getElementById("level-1");
+const levelTwo = document.getElementById("level-2");
+const levelThree = document.getElementById("level-3");
 
+// Assegno la variabile per stampare in HTML
+const containerHtml = document.getElementById("container");
 
-levels(outputHtml, 'button-1', 'Level 1');
-levels(outputHtml, 'button-2', 'Level 2');
-levels(outputHtml, 'button-3', 'Level 3');
+// Creo un evento al click per ogni button e assegno il numero di box da creare richiamando la funzione
+levelOne.addEventListener('click', function (){
+    buildGrid(containerHtml, 100, 'width-level-1');  
+});
+
+levelTwo.addEventListener('click', function (){
+    buildGrid(containerHtml, 81, 'width-level-2');
+});
+
+levelThree.addEventListener('click', function (){
+    buildGrid(containerHtml, 49, 'width-level-3');
+});
